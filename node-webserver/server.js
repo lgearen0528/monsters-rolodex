58ba52774ac79cd1638c5f41f8ee5f17352cd2e1
Gearen Liem J <GearenLiemJ@JohnDeere.com>
2018-12-18 12:25:52 -0600

    Express Partitions, Middleware, and Helper exploration

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,23 +1,52 @@
 const express = require('express');
-
+const hbs = require('hbs'); //template for dynamic pages
+const fs = require('fs');
 //create express app
 let app = express();
 
-//routing
-app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name:'Liem',
-    likes:[
-      'apples',
-      'hiking',
-      'ribs'
-    ]
+hbs.registerPartials(__dirname + '/views/partials');
+
+//middleware, configure express app
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
   });
+  next();
 });
 
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public')); //need absolute paths
+
+//helper functions
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+//routing
 app.get('/about', (req, res) =>{
-  res.send('<h1>About Page</h1>');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my site!'
+  })
 });
 
 app.get('/bad', (req, res) => {
@@ -25,5 +54,7 @@ app.get('/bad', (req, res) => {
     errorMessage: 'Error handling request!'
   })
 });
-//go to localhost:3000
-app.listen(3000);
+//go to localhost:3000, log message that it's up
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
