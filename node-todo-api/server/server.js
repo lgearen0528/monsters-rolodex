ecb7861b3d6bf6d8a9b36986895fe6fbef81def4
Gearen Liem J <GearenLiemJ@JohnDeere.com>
2019-01-02 13:57:37 -0600

    add GET /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -24,6 +24,19 @@ app.post('/todos', (req, res) => {
   })
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+
+
+
+
 //export for testing
 module.exports = {app};
 
