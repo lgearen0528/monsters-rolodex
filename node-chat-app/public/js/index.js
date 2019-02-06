087e0d0116a91392583299257759aa419c49717c
Gearen Liem J <GearenLiemJ@JohnDeere.com>
2019-02-06 14:04:20 -0600

    added html templating for messages

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,7 +1,7 @@
 let socket = io();
 
 socket.on('connect', function () {
-  console.log('connected to server');
+  console.log('Connected to server.');
 
 
   // socket.emit('createEmail', {
@@ -24,10 +24,20 @@ socket.on('disconnect', function (){
 
 socket.on('newMessage', function(message){
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  let template = jQuery('#message-template').html();
+  let html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+
+  // let formattedTime = moment(message.createdAt).format('h:mm a');
+  // let li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 
@@ -43,13 +53,21 @@ jQuery('#message-form').on('submit', function (e) {
 
 });
 socket.on('newLocationMessage', function (message) {
-  let li = jQuery('<li></li>');
-  let a = jQuery('<a target="_blank">My current location</a>');
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  let template = jQuery('#location-message-template').html();
+  let html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
+  jQuery('#messages').append(html);
+  // let li = jQuery('<li></li>');
+  // let a = jQuery('<a target="_blank">My current location</a>');
+  // let formattedTime = moment(message.createdAt).format('h:mm a');
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 let locationButton = jQuery('#send-location');
 locationButton.on('click', function(){
